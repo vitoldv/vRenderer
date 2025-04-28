d7a74288e57c24a072587e623e3564e9307f3911
VITOLD DESKTOP <v.vitaliy.vishnyakov@gmail.com>
2025-04-28 17:26:15 +0300

    added basic code for controllable frame rendering

--- vRenderer/src/main.cpp
+++ vRenderer/src/main.cpp
@@ -7,7 +7,16 @@
 #define WINDOW_WIDTH		1920
 #define WINDOW_HEIGHT		1080
 
+#define FPS_LIMIT			false
+#define TARGET_FPS			60
+#define TARGET_FRAME_TIME	(1000 / TARGET_FPS)
+
 GLFWwindow* window;
+
+// Frame time control 
+int previousFrameTime = 0;
+int currentFrameTime = 0;
+float deltaTime = 0;
 
 void initWindow(std::string title, const int width, const int height)
 {
@@ -19,16 +28,45 @@ void initWindow(std::string title, const int width, const int height)
 	window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 }
 
+void destroyWindow()
+{
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void processInput()
+{
+}
+
+void update()
+{
+}
+
+void render()
+{
+}
+
 int main()
 {
 	initWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+	float frameTime = 0;
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		currentFrameTime = glfwGetTime() * 1000.0f;
+		frameTime = currentFrameTime - previousFrameTime;
+
+		if (FPS_LIMIT && frameTime < TARGET_FRAME_TIME) continue;
+
+		deltaTime = frameTime / 1000.0f;
+		previousFrameTime = currentFrameTime;
+
+		processInput();
+		update();
+		render();
 	}
 
-	// Destroy window
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	destroyWindow();
 }
